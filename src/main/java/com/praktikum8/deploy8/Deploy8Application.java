097f83550e0e758eadf1faf7c6924332d25bb68c
Lenovo <9.3razzinayyuri@gmail.com>
2026-04-20 14:05:26 +0700

    Initial commit

---
+++ src/main/java/com/praktikum8/deploy8/Deploy8Application.java
@@ -0,0 +1,13 @@
+package com.praktikum8.deploy8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktikum8/deploy8/Deploy8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum8.deploy8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
